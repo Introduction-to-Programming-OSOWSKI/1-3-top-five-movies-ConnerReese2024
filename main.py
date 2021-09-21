9786b239f5f4afb376d441c9acf9dcda29413faf
ConnerReese2024 <820449@mystma.org>
2021-09-21 09:34:57 -0500

    3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def topFiveMovies(GodzillaKingOfTheMonsters, GodzillavsKong, ThorRagnarok, BabyDriver, TerminatorDarkFate):
-    return GodzillaKingOfTheMonsters, GodzillavsKong, ThorRagnarok, BabyDriver, TerminatorDarkFate
+def topFiveMovies(movie1, movie2, movie3, movie4, movie5):
+    return GodzillaKingOfTheMonsters + GodzillavsKong + ThorRagnarok + BabyDriver + TerminatorDarkFate
 
 print (topFiveMovies("GodzillaKingOfTheMonsters", "GodzillavsKong", "ThorRagnarok", "BabyDriver", "TerminatorDarkFate"))
