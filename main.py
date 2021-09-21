a6f565c8c7d6d85941588559f9ee2402d0f9cc8a
ConnerReese2024 <820449@mystma.org>
2021-09-21 09:40:00 -0500

    3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def topFiveMovies(movie1, movie2, movie3, movie4, movie5):
-    return movie1 + movie2 + movie3 + movie4 + movie5
+    return movie1, movie2, movie3, movie4, movie5
 
 print (topFiveMovies("GodzillaKingOfTheMonsters", "GodzillavsKong", "ThorRagnarok", "BabyDriver", "TerminatorDarkFate"))
